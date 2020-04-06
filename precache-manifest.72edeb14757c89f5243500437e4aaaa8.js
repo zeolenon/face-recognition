d6b237066c315b2fe0de3ea18d8dc81de8a33f8e
Leo Zenon <leozenon@ufrn.edu.br>
2020-04-05 21:46:46 -0300

    Updates

---
+++ precache-manifest.72edeb14757c89f5243500437e4aaaa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0850368bd900519be5da18bbad03422d",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "a6ac8b532bbd43423147",
+    "url": "/smart-brain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "c78ed956a997c529593e",
+    "url": "/smart-brain/static/css/main.f0c0e5e7.chunk.css"
+  },
+  {
+    "revision": "a6ac8b532bbd43423147",
+    "url": "/smart-brain/static/js/2.de5fc366.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brain/static/js/2.de5fc366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c78ed956a997c529593e",
+    "url": "/smart-brain/static/js/main.3dab486a.chunk.js"
+  },
+  {
+    "revision": "07687dc78c01b19d353b",
+    "url": "/smart-brain/static/js/runtime-main.cf82cd7f.js"
+  }
+]);
